eeb7e05fffc2837845e7095c832a9d0c5f159c5a
Andrei Freund <freund.andrei@gmail.com>
2021-09-27 17:58:25 -0500

    client done

--- Iprefer.java
+++ Iprefer.java
@@ -3,6 +3,14 @@
  * avfreund@wisc.edu
  * cs-login: andrei
  */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 public class Iprefer{
     public static void main(String args[]){
         if(args.length == 0){
@@ -67,5 +75,41 @@ public class Iprefer{
     private static void invalid(){
             System.out.println("Error: invalid arguments");
             System.exit(1);
+    }
+
+    /*
+     * Client Class
+     * Represents a client 
+     */
+    private class Client{
+        private int port;
+        private String ip;
+        private Client(String ip, int port, int time){
+            this.ip = ip;
+            this.port = port;
+            run(time);
+        }
+        private void run(int time){
+            Socket s;
+            String msg = new String(new byte[1000], StandardCharsets.UTF_8); // 1000 byte 0 filled message
+            long sent = 0;
+            try {
+                s = new Socket(ip,port);
+                PrintWriter p = new PrintWriter(s.getOutputStream(),true);
+                long stop = (long) time * 1000 + System.currentTimeMillis();
+                while (System.currentTimeMillis() < stop){
+                    p.println(msg);
+                    sent++;
+                }
+                p.close();
+                s.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+				System.exit(1);
+            }
+            double r = (sent * 8) / 1000.0 / time;
+			System.out.printf("sent=" + sent + " KB rate=%.3f Mbps\n", r);
+            
+        }
     }
 }
